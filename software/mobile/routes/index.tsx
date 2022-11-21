b569cafde1eb68a66175c15c8851d0c652c5281f
Paul Okenne <okennepaul@gmail.com>
2022-11-21 16:17:53 -0500

    fix: add forgot password to navigation

--- software/mobile/routes/index.tsx
+++ software/mobile/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
 import LoginScreen from "../screens/LoginScreen";
 import SignupScreen from "../screens/SignupScreen";
 import { RootStackParamList } from "../types";
@@ -15,6 +16,8 @@ export const RootNavigation = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+
         <Stack.Screen name="Main" component={BottomTabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
