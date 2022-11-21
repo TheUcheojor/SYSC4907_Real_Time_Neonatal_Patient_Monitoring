d00d527ad2cf0de245e3e301cd9243eaf62bae3b
Paul Okenne <okennepaul@gmail.com>
2022-11-21 16:19:43 -0500

    feat: creat component for app icon

---
+++ software/mobile/components/LargeAppIcon.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+export const APP_ICON: string = "baby";
+
+export default () => <FontAwesome5 name={APP_ICON} size={100} color="black" />;
