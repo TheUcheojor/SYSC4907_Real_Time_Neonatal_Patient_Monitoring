b0cec7704f29f3b579adf5de4f8802b9c33586dc
Paul Okenne <okennepaul@gmail.com>
2022-11-21 16:18:29 -0500

    refact: add file to store photos and constants

---
+++ software/mobile/constants/Images.tsx
@@ -0,0 +1,3 @@
+import { ImageSourcePropType } from "react-native";
+
+export const VIBRATION_ICON: ImageSourcePropType = require("../assets/images/shake.png");
--- software/mobile/constants/ViewConstants.tsx
+++ software/mobile/constants/ViewConstants.tsx
@@ -1,8 +1,15 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { RootStackParamList } from "../types";
+
 /**
  * Global Constants
  */
-export const APP_ICON: string = "baby";
-export const BLACK_COLOR: string = "#000000";
+export const APP_NAME: string = "Transport Comfort Analysis";
+export const LOGIN_PAGE: keyof RootStackParamList = "Login";
+
+// The name of a collection that contains core pages
+export const MAIN_PAGE_COLLECTECTION: keyof RootStackParamList = "Main";
 
 /**
  * Login View Constants
@@ -14,3 +21,21 @@ export const EMAIL_PLACEHOLDER: string = "Your Email...";
 
 export const PASSWORD_TITLE: string = "Password";
 export const PASSWORD_PLACEHOLDER: string = "Your Password...";
+
+export const FORGOT_PASSWORD_TEXT: string = "Forgot password?";
+export const NO_ACCOUNT_SIGNUP_TEXT: string = "No account? Signup here";
+
+export const LOGIN_BUTTON_TEXT = "Login";
+
+/**
+ * Forgot Password View Constants
+ */
+
+export const FORGOT_PASSWORD_HEADER: string = "Forgot Password";
+export const SEND_TEMPORARY_PASSOWRD_BUTTON_TEXT = "Send Temporary Password";
+
+/**
+ * Paramedic View
+ */
+
+export const VIBRATION_ICON_PATH: string = "../assets/images/shake.png";
