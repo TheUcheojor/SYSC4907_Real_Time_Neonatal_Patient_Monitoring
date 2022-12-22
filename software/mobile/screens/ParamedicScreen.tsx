5502a6f579ebd8a8e8efcd50dcecc7ed619c2c2f
Paul Okenne <okennepaul@gmail.com>
2022-12-22 15:54:03 -0500

    chore: remove old ios build

--- software/mobile/screens/ParamedicScreen.tsx
+++ software/mobile/screens/ParamedicScreen.tsx
@@ -11,7 +11,7 @@ export default ({
 }: NativeStackScreenProps<MainStackParamList, "Paramedic">): JSX.Element => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <MetricLiveView></MetricLiveView>
+      <MetricLiveView />
     </View>
   );
 };
