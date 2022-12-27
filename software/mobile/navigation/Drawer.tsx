b6e53a200590fda7a088e1e7c85ee6ea9dd38486
Paul Okenne <okennepaul@gmail.com>
2022-12-26 23:31:19 -0500

    refact: update drawer type across platforms

--- software/mobile/navigation/Drawer.tsx
+++ software/mobile/navigation/Drawer.tsx
@@ -48,6 +48,7 @@ export default () => {
         headerShown: false,
         drawerPosition: "right",
         swipeEnabled: false,
+        drawerType: "front",
       }}
     >
       <Drawer.Screen name="BottomTab" component={BottomTabNavigator} />
