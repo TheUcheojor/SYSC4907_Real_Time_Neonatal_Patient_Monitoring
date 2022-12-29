b1f92512ddbb6309d63631b8b7c7ca11141a5c0e
Paul Okenne <okennepaul@gmail.com>
2022-12-28 21:27:35 -0500

    feat: add loading page
    - stays until the database is configured and fonts are loaded

--- software/mobile/App.tsx
+++ software/mobile/App.tsx
@@ -4,7 +4,7 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StackNavigationContainer } from "./navigation/StackNavigationContainer";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import {
   useFonts,
   Montserrat_600SemiBold_Italic,
@@ -16,18 +16,35 @@ import {
 } from "@expo-google-fonts/montserrat";
 
 import { LogBox } from "react-native";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { RecordingState } from "./components/TripRecorder";
+import { DatabaseController } from "./controllers/database/DatabaseController";
+import AppIcon from "./components/AppIcon";
 
 LogBox.ignoreLogs(["new NativeEventEmitter"]); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default function App() {
+  const [isDatabaseLoaded, setDatabaseLoaded] = useState<boolean>(false);
+
   const [recordingState, setRecordingState] = useState<RecordingState>(
     RecordingState.NOT_RECORDING
   );
 
-  let [fontsLoaded] = useFonts({
+  //Load the database
+  const loadDatabase = useCallback(async () => {
+    await DatabaseController.getConfiguredDatabaseController();
+    setDatabaseLoaded(true);
+
+    // console.log("here");
+  }, []);
+
+  useEffect(() => {
+    loadDatabase();
+  }, [loadDatabase]);
+
+  // Load the fonts
+  let [fontsLoaded]: [boolean, Error | null] = useFonts({
     Montserrat_600SemiBold_Italic,
     Montserrat_600SemiBold,
     Montserrat_700Bold,
@@ -36,20 +53,38 @@ export default function App() {
     Montserrat_800ExtraBold,
   });
 
-  return fontsLoaded ? (
-    // <GestureHandlerRootView style={{ flex: 1 }}>
+  if (!isDatabaseLoaded || !fontsLoaded) {
+    return (
+      <View style={styles.loadingPageContainer}>
+        <AppIcon size={400} />
+        <Text style={styles.title}>TCA</Text>
+      </View>
+    );
+  }
+
+  return (
     <SafeAreaProvider>
       <StackNavigationContainer
         recordingState={recordingState}
         setRecordingState={setRecordingState}
       />
-
       <StatusBar />
     </SafeAreaProvider>
-  ) : (
-    // </GestureHandlerRootView>
-    // <GestureHandlerRootView>
-    <View></View>
-    // </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingPageContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    margin: 10,
+    letterSpacing: 5,
+  },
+});
