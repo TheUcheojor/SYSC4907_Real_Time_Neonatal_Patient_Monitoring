fd73fa89968f9daf8270810cfe69837f8fd9909e
rfife <rblf100@gmail.com>
2023-01-15 14:47:06 -0500

    style: move regex

--- software/website/src/components/modal/ForgotPasswordModalContent.tsx
+++ software/website/src/components/modal/ForgotPasswordModalContent.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useRef } from "react";
 import LoadingIcon from "components/icons/LoadingIcon";
 import { ColorEnum } from "constants/ColorEnum";
-
-const validEmailRegex =
-  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+import { VALID_EMAIL_REGEX } from "constants/Auth";
 
 function ForgotPasswordModalContent() {
   const email = useRef("");
@@ -41,7 +39,7 @@ function ForgotPasswordModalContent() {
   }
 
   function handleKeyUp() {
-    let regexMatches = email.current.trim().match(validEmailRegex);
+    let regexMatches = email.current.trim().match(VALID_EMAIL_REGEX);
     let regexMatch =
       regexMatches != null && regexMatches.length > 0 ? regexMatches[0] : null;
     if (regexMatch != null && regexMatch.length === email.current.trim().length)
@@ -55,7 +53,7 @@ function ForgotPasswordModalContent() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        maxWidth: "305px"
+        maxWidth: "305px",
       }}
     >
       {requestResetResult !== "" && (
--- software/website/src/components/modal/SignUpModalContent.tsx
+++ software/website/src/components/modal/SignUpModalContent.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "css/App.css";
 import LoadingIcon from "components/icons/LoadingIcon";
 import { ColorEnum } from "constants/ColorEnum";
-import { PASSWORD_LENGTH_MIN } from "constants/Auth";
+import { PASSWORD_LENGTH_MIN, VALID_EMAIL_REGEX } from "constants/Auth";
 
 function SignUpModalContent() {
   const [fullName, setFullName] = useState("");
@@ -13,10 +13,7 @@ function SignUpModalContent() {
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
   const [isEnabled, setIsEnabled] = useState(false);
-
-  const VALID_EMAIL_REGEX =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
-
+  
   function handleSignUp() {
     setIsFetching(true);
     setIsEnabled(false);
