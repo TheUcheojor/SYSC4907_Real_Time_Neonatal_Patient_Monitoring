0e649cc7e3c49c314bef8356ddb92f6c4eb48098
Paul Okenne <okennepaul@gmail.com>
2023-02-18 18:29:29 -0500

    feat: add communication error enum

---
+++ software/mobile/services/models/error-handling/CommunicationError.tsx
@@ -0,0 +1,9 @@
+/**
+ * Author: Paul Okenne
+ * File: CommunicationErrors
+ * Purpose: exports an enu containing communication errors
+ */
+
+export enum CommunicationError {
+  SESSION_EXPIRED = "Session does not exist",
+}
